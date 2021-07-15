95786c3d1e458908bfc25be9e6571873f6477861
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-15 14:16:39 -0700

    built out state

--- src/API.ts
+++ src/API.ts
@@ -15,18 +15,20 @@ question: string;
 type: string;
 }
 
-export type QuestionType = Question & { answers: string[]}
+export type QuestionState = Question & { answers: string[]}
 
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+export const fetchQuizQuestions = async (
+    amount: number, 
+    difficulty: Difficulty
+    ) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    return data.results.map((question: Question) => (
-        {
+    return data.results.map((question: Question) => ({
             ...question,
-            answer: shuffleArray([
+            answers: shuffleArray([
                 ...question.incorrect_answers, 
-                question.correct_answer])
-        }
-    ))
+                question.correct_answer
+            ])
+        }))
 }
