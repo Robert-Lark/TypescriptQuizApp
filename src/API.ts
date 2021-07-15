857acc92d9cb92369e1f3423aae0667bc63f06c9
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-15 10:06:31 -0700

    added fetch command

--- src/API.ts
+++ src/API.ts
@@ -1,7 +1,11 @@
-import React from 'react';
-
-function API() {
-    return ;
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = 'medium',
+    HARD = 'hard',
 }
 
-export default API;
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
+import  { fetchQuizQuestions} from './API'
 //Components
 import QuestionCard from './components/QuestionCard';
-
+//types
+import { Difficulty } from './API'
 const TOTAL_QUESTIONS = 10
 
 function App() {
@@ -11,6 +13,8 @@ const [number, setNumber] = useState(0)
 const [userAnswers, setUserAnswers] = useState([])
 const [score, setScore] = useState(0)
 const [gameOver, setGameOver] = useState(false)
+
+console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
 const startTrivia = async () => {
 
@@ -33,14 +37,14 @@ Start
       </button>
       <p className='score'>Score:</p>
       <p>Loading Questions ... </p>
-      <QuestionCard
+      {/* <QuestionCard
        questionNr={number + 1}
        totalQuestions={TOTAL_QUESTIONS}
        question={questions[number].question}
        answers={questions[number].answers}
        userAnswer={userAnswers ? userAnswers[number] : undefined}
        callback={checkAnswer}
-       />
+       /> */}
       <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
   );
